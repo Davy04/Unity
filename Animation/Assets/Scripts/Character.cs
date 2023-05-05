ad1d1b4756d9f14e7fa3096d9314a8b371ec401b
Davy Woolley <davywoolley@gmail.com>
2023-05-05 16:04:36 -0300

    anim

--- Animation/Assets/Scripts/Character.cs
+++ Animation/Assets/Scripts/Character.cs
@@ -10,11 +10,12 @@ public class Character : MonoBehaviour
     public float gravity = 20;
     public Vector3 moveDir = Vector3.zero;
     public CharacterController control;
+    public Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -36,6 +37,20 @@ public class Character : MonoBehaviour
         rotation *= Time.deltaTime;
 
         transform.Rotate(0, rotation, 0);
+
+        if (control.isGrounded)
+        {
+            if (moveDir.z != 0)
+            {
+                anim.SetBool("stop", false);
+                anim.SetBool("walking", true);
+            }
+            else
+            {
+                anim.SetBool("stop", true);
+                anim.SetBool("walking", false);
+            }
+        }
 
     }
 }
