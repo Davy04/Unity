3d126d1d744bbda89550a28b96fca13f135790e6
Davy Woolley <davywoolley@gmail.com>
2023-05-15 18:17:27 -0300

    UI and Animation

--- Animation/Assets/Scripts/TriggerDestroy.cs
+++ Animation/Assets/Scripts/TriggerDestroy.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TriggerDestroy : MonoBehaviour
 {
+    [SerializeField]
+    private int itens = 0;
+    [SerializeField]
+    private Text Txt;
+
     void OnTriggerEnter(Collider col)
     {
-        if (col.gameObject.CompareTag("Player"))
+        if (col.gameObject.CompareTag("Coin"))
         {
-            Destroy(gameObject);
+            Destroy(col.gameObject);
+            itens += 1;
+            Txt.text = itens.ToString();
         }
     }
 }
